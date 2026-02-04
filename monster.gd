3f8be5addc0238935be38bf40ddb0195691b7ee8
DanyloNevidomyi <149074999+DanyloNevidomyi@users.noreply.github.com>
2026-02-04 17:50:05 +0200

    Added player and monster
    Warning! For now this game starts only when you manually start from scene "level1"
    Added:
    - player and player scene
    - monster and monster scene
    - added Level1
    Fixed:
    - nothing, just ruined
    ToDo:
    - fix monster's following
    - change to smaller sprites
    - add monster's catch

---
+++ monster.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var speed = 150.0
+@export var player: Node2D # Це "порожнє місце", куди ми пізніше покладемо гравця
+
+func _physics_process(delta):
+	# Монстр рухається тільки якщо ми сказали йому, хто такий "player"
+	if player:
+		# Дивимось, де гравець, і йдемо до нього
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * speed
+		move_and_slide()
