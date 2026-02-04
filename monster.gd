052660063537a102722f022803e86f4cf4c8a7f4
UaBoy <73942485+lavandaya@users.noreply.github.com>
2026-02-04 19:20:00 +0200

    Minor changes
    - Fixed:
    * Start button
    * Sprite size
    * Collision

--- monster.gd
+++ monster.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var speed = 150.0
 @export var player: Node2D # Це "порожнє місце", куди ми пізніше покладемо гравця
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Монстр рухається тільки якщо ми сказали йому, хто такий "player"
 	if player:
 		# Дивимось, де гравець, і йдемо до нього
--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var speed = 300.0 # Швидкість бігу
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Отримуємо натискання кнопок (вліво, вправо, вгору, вниз)
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
