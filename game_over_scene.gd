7359eea8f912d61b9b95ba1b434eb5bb52a6a87e
DanyloNevidomyi <149074999+DanyloNevidomyi@users.noreply.github.com>
2026-02-05 16:08:28 +0200

    Updated monster mechanics
    Added:
    - Monster catches player
    - Game over scene
    Fixed:
    - Deleted monster and player scenes from mainMenu
    ToDo:
    - honestly idk man, i just doing what i like

---
+++ game_over_scene.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	# Знаходимо плеєр (через знак $ звертаємось до дитини)
+	# Підключаємо сигнал "finished" до нашої функції
+	$VideoStreamPlayer.finished.connect(_on_video_finished)
+
+func _on_video_finished():
+	# Коли відео закінчилось -> вантажимо Головне Меню
+	get_tree().change_scene_to_file("res://mainMenu.tscn") # Перевір назву свого файлу меню!
--- monster.gd
+++ monster.gd
@@ -10,3 +10,8 @@ func _physics_process(_delta):
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction * speed
 		move_and_slide()
+
+
+func _on_kill_zone_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://game_over_scene.tscn")
