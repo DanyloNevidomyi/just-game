003ced802b8b95ec2f1981d21f1822da76b31f53
DanyloNevidomyi <149074999+DanyloNevidomyi@users.noreply.github.com>
2026-02-12 17:21:15 +0200

    Improved level 1
    Added:
    - background
    - improved monster navigation
    Fixed:
    - nothing
    ToDo:
    - create level 2
    - add transition from level 1 to level 2

--- script/monster.gd
+++ script/monster.gd
@@ -1,17 +1,29 @@
 extends CharacterBody2D
 
 var speed = 150.0
-@export var player: Node2D # Це "порожнє місце", куди ми пізніше покладемо гравця
+@export var player: Node2D
 
-func _physics_process(_delta):
-	# Монстр рухається тільки якщо ми сказали йому, хто такий "player"
-	if player:
-		# Дивимось, де гравець, і йдемо до нього
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction * speed
-		move_and_slide()
+# Отримуємо посилання на наш "GPS" (ноду, яку ми додали на Кроці 2)
+@onready var nav_agent := $NavigationAgent2D
 
+func _physics_process(delta):
+	# Якщо гравця немає - стоїмо
+	if !player:
+		return
+		
+	# 1. Кажемо агенту: "Ми хочемо дійти до гравця"
+	nav_agent.target_position = player.global_position
+	
+	# 2. Агент рахує шлях і каже, де наступна точка повороту
+	var next_path_position = nav_agent.get_next_path_position()
+	
+	# 3. Рухаємось не до гравця, а до цієї НАСТУПНОЇ точки
+	var direction = global_position.direction_to(next_path_position)
+	velocity = direction * speed
+	
+	move_and_slide()
 
-func _on_kill_zone_body_entered(body: Node2D) -> void:
+# Тут твій старий код для вбивства (залиш його без змін)
+func _on_kill_zone_body_entered(body):
 	if body.name == "Player":
-		get_tree().call_deferred("change_scene_to_file", "res://scene/game_over_scene.tscn")
+		get_tree().change_scene_to_file("res://scene/game_over_scene.tscn")
